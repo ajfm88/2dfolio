75c028ebb140d4753c2ee4b7b57fa2a01335a7c6
ａｊｆｍ８８ <ale@ajfm88.com>
2026-03-14 11:59:39 -0700

    29. Birthday update — Major portfolio overhaul:   - Migrate from Kaboom.js to Kaplay (updated package.json, npm install, kaboomCtx.js import).   - Add separate nightstand boundary and dialogue in map.json and constants.js.   - Update projects dialogue to reference Kaplay with a link to kaplayjs.com.   - Rename kaboomCtx.js to kaplayCtx.js and updated the import in main.js.   - Add Goodreads link.   - Add new game link.

--- home/src/constants.js
+++ home/src/constants.js
@@ -4,27 +4,31 @@ export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a few games in that language. I like Python as well. Anyways, regardless of the language, I just like programming.
     Here is my <a href="https://github.com/ajfm88" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud to have it. It was a very theoretical degree but I think it gave me a good foundation.
-    I am also currently enrolled in a Master's degree in CS at a different university to keep expanding my skillset.`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud to have it. It was a very theoretical degree but I think it gave 
+    me a good foundation. I am also currently enrolled in a Master's degree in CS at a different university to keep expanding my skillset.`,
   "sofa-table": `That's my sofa and my table. I like to relax here and sometimes watch YouTube, or read a good manga in Japanese.
-    In my <a href="https://ankiweb.net/shared/by-author/215281557" target="_blank">AnkiWeb page</a> you'll find some of the Anki decks I have assembled to study Japanese with.`,
+    In my <a href="https://ankiweb.net/shared/by-author/215281557" target="_blank">AnkiWeb page</a> you'll find some of the Anki decks I 
+    have assembled to study Japanese with.`,
   tv: `There is a video game console here, which game will I play?: 
   <a href="https://metroidvania-ajfm88.netlify.app" target="_blank">Action</a>,
   <a href="https://pizza-legends-ajfm88.netlify.app" target="_blank">JRPG</a>,
+  <a href="https://ninja-ajfm88.netlify.app" target="_blank">Ninja Adventure</a>,
   <a href="https://super-mango-ajfm88.netlify.app" target="_blank">Platformer</a>,
   <a href="https://html-classic.itch.zone/html/7730457/build/index.html" target="_blank">Puzzle</a>,
   <a href="https://necro-ajfm88.netlify.app" target="_blank">Roguelite</a>,
   <a href="https://sonic-runner-ajfm88.netlify.app" target="_blank">Runner</a>,
   <a href="https://duck-hunter-ajfm88.netlify.app" target="_blank">Shooter</a>,
   <a href="https://zelda-ajfm88.netlify.app" target="_blank">Zelda Adventure</a>.`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I occasionally like writing restaurant reviews, please check out my 
-    <a href="https://www.yelp.com/user_details?userid=JBqCl4WE7g9SPR-0y0tJzQ" target="_blank">Yelp profile</a> if you'd like to.`,
+  nightstand: `This is my nightstand, where I keep my cellphone. I occasionally like writing restaurant reviews, please check out 
+    my <a href="https://www.yelp.com/user_details?userid=JBqCl4WE7g9SPR-0y0tJzQ" target="_blank">Yelp profile</a> if you'd like to.`,
+  bed: `This is where I sleep. I try to get a solid 7 to 8 hours of sleep, as it helps me stay anabolic.`,
   resume: `This is my desk and on it is my resume. <a href="https://tr.ee/Xj8r3OZd11" target="_blank">Check it out?</a>
     Contact me at ale@ajfm88.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of the tutorials from this <a href="https://youtube.com/@jslegenddev" target="_blank">YouTube channel</a>.`,
+  projects: `Info about this portfolio : It's made with the <a href="https://kaplayjs.com/" target="_blank">Kaplay</a> 
+    library which is a library for making games in JavaScript. Text is rendered with HTML/CSS. So the textbox you're currently reading is 
+    not rendered within canvas..`,
   library: `There are a some programming books on my shelves. There are also the ESV Study Bible, the HCSB Study Bible (I read them both regularly), 
-    books on the Christian faith and intermediate-level Japanese language textbooks.`,
+    books on the Christian faith, some novels, short story collections and even some intermediate-level Japanese language textbooks.
+    Here is my <a href="https://goodreads.com/ajfm88" target="_blank">Goodreads profile</a> to see what I've been reading lately.`,
   exit: `If you want to exit ajfm88's Home, just close the tab.`,
 };
