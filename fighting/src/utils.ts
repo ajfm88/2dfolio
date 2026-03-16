c9e9789bdb9aced37b41ba12cc134afc55800b53
ａｊｆｍ８８ <ale@ajfm88.com>
2026-03-16 13:48:10 -0700

    02. Write logic for displaying maps from Tiled.

--- fighting/src/utils.ts
+++ fighting/src/utils.ts
@@ -1,1 +1,42 @@
-function loadMap() {}
+import { KaboomCtx, GameObj } from "kaboom";
+import { TiledLayer } from "./types";
+
+export async function fetchMapData(mapFilePath: string) {
+  const response = await fetch(mapFilePath);
+  if (response.status === 404) {
+    throw new Error(response.statusText);
+  }
+
+  return await response.json();
+}
+
+export function drawTiles(
+  k: KaboomCtx,
+  map: GameObj,
+  layer: TiledLayer,
+  tilewidth: number,
+  tileheight: number
+) {
+  if (!layer.data) return;
+
+  let nbOfDrawnTiles = 0;
+  const tilePos = k.vec2(0, 0);
+  for (const tile of layer.data) {
+    if (nbOfDrawnTiles % layer.width === 0) {
+      tilePos.x = 0;
+      tilePos.y += tileheight;
+    } else {
+      tilePos.x += tilewidth;
+    }
+
+    nbOfDrawnTiles++;
+
+    if (tile === 0) continue;
+
+    map.add([
+      k.sprite("tileset", { frame: tile - 1 }),
+      k.pos(tilePos),
+      k.offscreen(),
+    ]);
+  }
+}
