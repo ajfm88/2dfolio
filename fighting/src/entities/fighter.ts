4e2669073c6d0538ae351eba1a4215278f49c6a9
ａｊｆｍ８８ <ale@ajfm88.com>
2026-03-23 12:33:15 -0700

    09. Work on sword fighting logic. Make sword animation take precedence over other animations.

--- fighting/src/entities/fighter.ts
+++ fighting/src/entities/fighter.ts
@@ -1,4 +1,10 @@
-import { GameObj, KaboomCtx } from "kaboom";
+import { GameObj, KaboomCtx, Vec2 } from "kaboom";
+
+export const fighterProps = {
+  speed: 200,
+  direction: null,
+  isSwordAnimPlaying: false,
+};
 
 export function setFighterControls(
   k: KaboomCtx,
@@ -6,16 +12,20 @@ export function setFighterControls(
   keys: { LEFT: string; RIGHT: string; UP: string; DOWN: string }
 ) {
   k.onKeyDown((key) => {
+    if (fighter.isSwordAnimPlaying) return;
+
     switch (key) {
       case keys.LEFT:
         fighter.flipX = true;
         fighter.move(-fighter.speed, 0);
         if (fighter.curAnim() !== "run") fighter.play("run");
+        fighter.direction = "LEFT";
         break;
       case keys.RIGHT:
         fighter.flipX = false;
         fighter.move(fighter.speed, 0);
         if (fighter.curAnim() !== "run") fighter.play("run");
+        fighter.direction = "RIGHT";
         break;
       default:
     }
@@ -28,11 +38,26 @@ export function setFighterControls(
         break;
       case keys.DOWN:
         if (fighter.curAnim() !== "attack") {
+          const fighterPos = fighter.worldPos();
+          const hitboxPos: { [key: string]: Vec2 } = {
+            LEFT: k.vec2(fighterPos.x - 50, fighterPos.y),
+            RIGHT: k.vec2(fighterPos.x + 50, fighterPos.y),
+          };
+
+          const attackHitbox = k.add([
+            k.area({ shape: new k.Rect(k.vec2(0), 50, 50) }),
+            k.anchor("center"),
+            k.pos(hitboxPos[fighter.direction]),
+          ]);
+
           fighter.play("attack", {
             onEnd() {
               fighter.play("idle");
+              k.destroy(attackHitbox);
+              fighter.isSwordAnimPlaying = false;
             },
           });
+          fighter.isSwordAnimPlaying = true;
         }
         break;
       default:
