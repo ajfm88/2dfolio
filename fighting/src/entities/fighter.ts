71ffb3ddd76fad363914d81bcad30820d77fa1a7
ａｊｆｍ８８ <ale@ajfm88.com>
2026-03-28 13:24:04 -0700

    14. Implement working healthbar and version of fighter controls & anims.

--- fighting/src/entities/fighter.ts
+++ fighting/src/entities/fighter.ts
@@ -1,45 +1,37 @@
 import { GameObj, KaboomCtx, Vec2 } from "kaboom";
-import { Directions, Entity } from "../types";
+import { Directions } from "../types";
 
 export const fighterProps: {
   speed: number;
   direction: null | string;
-  isAttacking: boolean;
-  isRunning: boolean;
   isDead: boolean;
-  isHit: boolean;
   maxHp: number;
   previousHp: number;
   previousHeight: number;
 } = {
   speed: 200,
   direction: null,
-  isAttacking: false,
-  isRunning: false,
   isDead: false,
-  isHit: false,
   maxHp: 10,
   previousHp: 10,
   previousHeight: 0,
 };
 
-export function makeFighterBlink(k: KaboomCtx, fighter: GameObj) {
-  fighter.on("hurt", async () => {
-    await k.tween(
-      fighter.opacity,
-      0,
-      0.05,
-      (newOpacity) => (fighter.opacity = newOpacity),
-      k.easings.linear
-    );
-    await k.tween(
-      fighter.opacity,
-      1,
-      0.05,
-      (newOpacity) => (fighter.opacity = newOpacity),
-      k.easings.linear
-    );
-  });
+export async function makeFighterBlink(k: KaboomCtx, fighter: GameObj) {
+  await k.tween(
+    fighter.opacity,
+    0,
+    0.05,
+    (newOpacity) => (fighter.opacity = newOpacity),
+    k.easings.linear
+  );
+  await k.tween(
+    fighter.opacity,
+    1,
+    0.05,
+    (newOpacity) => (fighter.opacity = newOpacity),
+    k.easings.linear
+  );
 }
 
 export function setFighterControls(
@@ -47,133 +39,131 @@ export function setFighterControls(
   fighter: GameObj,
   keys: { LEFT: string; RIGHT: string; UP: string; DOWN: string }
 ) {
-  const movementControlsRef = k.onKeyDown((key) => {
-    switch (key) {
-      case keys.LEFT:
-        fighter.isRunning = true;
-        fighter.flipX = true;
-        fighter.move(-fighter.speed, 0);
-        fighter.direction = Directions.LEFT;
-        break;
-      case keys.RIGHT:
-        fighter.isRunning = true;
-        fighter.flipX = false;
-        fighter.move(fighter.speed, 0);
-        fighter.direction = Directions.RIGHT;
-        break;
-      default:
+  const onKeyDownListener = k.onKeyDown((key) => {
+    if (fighter.curAnim() === "attack") return;
+
+    if (key === keys.LEFT) {
+      fighter.flipX = true;
+      fighter.move(-fighter.speed, 0);
+      fighter.direction = Directions.LEFT;
+      if (fighter.curAnim() !== "run" && fighter.curAnim() !== "jump") {
+        fighter.play("run");
+      }
+      return;
+    }
+
+    if (key === keys.RIGHT) {
+      fighter.flipX = false;
+      fighter.move(fighter.speed, 0);
+      fighter.direction = Directions.RIGHT;
+      if (fighter.curAnim() !== "run" && fighter.curAnim() !== "jump") {
+        fighter.play("run");
+      }
+      return;
     }
   });
 
-  const actionsControlsRef = k.onKeyPress((key) => {
-    switch (key) {
-      case keys.UP:
-        if (fighter.isGrounded()) fighter.jump();
-        break;
-      case keys.DOWN:
-        fighter.isAttacking = true;
-
-        function updateHitboxPos() {
-          const fighterPos = fighter.worldPos();
-          const hitboxPos: { [key: string]: Vec2 } = {
-            LEFT: k.vec2(fighterPos.x - 50, fighterPos.y),
-            RIGHT: k.vec2(fighterPos.x + 50, fighterPos.y),
-          };
-          return hitboxPos[fighter.direction];
-        }
-
-        const attackHitbox = k.add([
-          k.area({ shape: new k.Rect(k.vec2(0), 50, 50) }),
-          k.anchor("center"),
-          k.pos(updateHitboxPos()),
-        ]);
-
-        const enemyTag = fighter.is("samurai") ? "ninja" : "samurai";
-
-        attackHitbox.onCollide(enemyTag, (enemy) => {
-          enemy.previousHp = enemy.hp();
-          k.wait(0.1, () => (enemy.previousHp = enemy.hp()));
-          if (enemy.hp() !== 0) {
-            enemy.hurt(1);
-            enemy.isHit = true;
-            return;
-          }
-
-          enemy.isDead = true;
-        });
-
-        const attackUpdateRef = k.onUpdate(() => {
-          attackHitbox.pos = updateHitboxPos();
-
-          if (!fighter.isAttacking) {
-            k.destroy(attackHitbox);
-            attackUpdateRef.cancel();
-          }
-        });
-
-        break;
-      default:
-    }
-  });
-
-  k.onKeyRelease(() => {
-    fighter.isRunning = false;
-  });
-
-  const animationManagerRef = k.onUpdate(() => {
-    if (fighter.isAttacking && fighter.curAnim() !== "attack") {
-      fighter.play("attack", {
-        onEnd() {
-          fighter.isAttacking = false;
-        },
-      });
-    }
-
-    if (fighter.isJumping() && fighter.curAnim() !== "jump")
-      fighter.play("jump");
-
-    if (fighter.isFalling() && fighter.curAnim() !== "fall")
-      fighter.play("fall");
-
+  const onKeyReleaseListener = k.onKeyRelease((key) => {
     if (
-      fighter.isRunning &&
-      !fighter.isJumping() &&
-      !fighter.isFalling() &&
-      !fighter.isAttacking &&
-      fighter.curAnim() !== "run"
-    )
-      fighter.play("run");
+      (key === keys.LEFT || key === keys.RIGHT) &&
+      fighter.curAnim() !== "idle"
+    ) {
+      fighter.play("idle");
+    }
+  });
 
-    if (fighter.isHit && fighter.curAnim() !== "hit") {
-      fighter.play("hit", {
-        onEnd() {
-          fighter.isHit = false;
-        },
-      });
+  const onKeyPressListener = k.onKeyPress((key) => {
+    if (
+      key === keys.UP &&
+      fighter.isGrounded() &&
+      fighter.curAnim() !== "jump"
+    ) {
+      fighter.jump();
+      fighter.play("jump");
     }
 
-    if (fighter.isDead && fighter.curAnim() !== "death") {
-      movementControlsRef.cancel();
-      actionsControlsRef.cancel();
+    if (key === keys.DOWN) {
+      function updateHitboxPos() {
+        const fighterPos = fighter.worldPos();
+        const hitboxPos: { [key: string]: Vec2 } = {
+          LEFT: k.vec2(fighterPos.x - 50, fighterPos.y),
+          RIGHT: k.vec2(fighterPos.x + 50, fighterPos.y),
+        };
+        return hitboxPos[fighter.direction];
+      }
 
-      // Needed since isGrounded need area comp to still exist or
-      // will crash game
+      const attackHitbox = k.add([
+        k.area({ shape: new k.Rect(k.vec2(0), 50, 50) }),
+        k.anchor("center"),
+        k.pos(updateHitboxPos()),
+      ]);
+
+      const enemyTag = fighter.is("samurai") ? "ninja" : "samurai";
+
+      attackHitbox.onCollide(enemyTag, (enemy) => {
+        k.wait(0.1, () => (enemy.previousHp = enemy.hp()));
+        if (enemy.hp() !== 0) {
+          enemy.hurt(1);
+          return;
+        }
+      });
+
+      const attackUpdateRef = k.onUpdate(() => {
+        attackHitbox.pos = updateHitboxPos();
+      });
+
+      if (fighter.curAnim() !== "attack") {
+        fighter.play("attack");
+      }
+
+      k.wait(0.3, () => {
+        k.destroy(attackHitbox);
+        attackUpdateRef.cancel();
+      });
+    }
+  });
+
+  fighter.on("hurt", () => {
+    makeFighterBlink(k, fighter);
+    if (fighter.hp() > 0 && fighter.curAnim !== "hit") {
+      fighter.play("hit");
+      return;
+    }
+
+    if (fighter.curAnim !== "death") {
+      fighter.isDead = true;
+      onKeyDownListener.cancel();
+      onKeyReleaseListener.cancel();
+      onKeyPressListener.cancel();
       fighter.isStatic = true;
       fighter.area.shape.height = 0;
-      fighter.play("death", {
-        onEnd() {
-          animationManagerRef.cancel();
-        },
-      });
+      fighter.play("death");
+    }
+  });
+
+  k.onUpdate(() => {
+    if (
+      !fighter.isJumping() &&
+      !fighter.isGrounded() &&
+      fighter.curAnim() !== "fall" &&
+      fighter.curAnim() !== "attack"
+    ) {
+      fighter.play("fall");
+    }
+
+    if (fighter.curAnim() === "fall" && fighter.isGrounded()) {
+      fighter.play("idle");
+      return;
     }
 
     if (
-      !fighter.isAttacking &&
-      !fighter.isRunning &&
-      fighter.isGrounded() &&
-      !fighter.isHit &&
-      !fighter.isDead &&
-      fighter.curAnim() !== "idle"
+      fighter.curAnim() !== "idle" &&
+      fighter.curAnim() !== "jump" &&
+      fighter.curAnim() !== "attack" &&
+      fighter.curAnim() !== "hit" &&
+      fighter.curAnim() !== "fall" &&
+      fighter.curAnim() !== "run" &&
+      !fighter.isDead
     ) {
       fighter.play("idle");
     }
